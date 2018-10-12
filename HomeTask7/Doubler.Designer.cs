19737f09f87c5bbd6a753516af51476fe1437f62
DonVolkRam <success.vka@gmail.com>
2018-10-12 14:26:13 +0300

    Предварительная версия удвоителя с выполенными заданиями

--- HomeTask7/Doubler.Designer.cs
+++ HomeTask7/Doubler.Designer.cs
@@ -38,11 +38,19 @@
             this.lblInfoTarget = new System.Windows.Forms.Label();
             this.lblInfoStep = new System.Windows.Forms.Label();
             this.lblStep = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.новаяИграToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.отменитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lbHistory = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnCommand1
             // 
-            this.btnCommand1.Location = new System.Drawing.Point(158, 12);
+            this.btnCommand1.Location = new System.Drawing.Point(181, 48);
             this.btnCommand1.Name = "btnCommand1";
             this.btnCommand1.Size = new System.Drawing.Size(75, 23);
             this.btnCommand1.TabIndex = 0;
@@ -52,7 +60,7 @@
             // 
             // btnCommand2
             // 
-            this.btnCommand2.Location = new System.Drawing.Point(158, 50);
+            this.btnCommand2.Location = new System.Drawing.Point(181, 86);
             this.btnCommand2.Name = "btnCommand2";
             this.btnCommand2.Size = new System.Drawing.Size(75, 23);
             this.btnCommand2.TabIndex = 1;
@@ -62,7 +70,7 @@
             // 
             // btnReset
             // 
-            this.btnReset.Location = new System.Drawing.Point(158, 86);
+            this.btnReset.Location = new System.Drawing.Point(181, 122);
             this.btnReset.Name = "btnReset";
             this.btnReset.Size = new System.Drawing.Size(75, 23);
             this.btnReset.TabIndex = 2;
@@ -74,7 +82,7 @@
             // 
             this.lblNumber.AutoSize = true;
             this.lblNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblNumber.Location = new System.Drawing.Point(82, 12);
+            this.lblNumber.Location = new System.Drawing.Point(77, 48);
             this.lblNumber.Name = "lblNumber";
             this.lblNumber.Size = new System.Drawing.Size(23, 25);
             this.lblNumber.TabIndex = 3;
@@ -82,7 +90,7 @@
             // 
             // btnPlay
             // 
-            this.btnPlay.Location = new System.Drawing.Point(158, 119);
+            this.btnPlay.Location = new System.Drawing.Point(181, 155);
             this.btnPlay.Name = "btnPlay";
             this.btnPlay.Size = new System.Drawing.Size(75, 23);
             this.btnPlay.TabIndex = 4;
@@ -94,7 +102,7 @@
             // 
             this.lblTarget.AutoSize = true;
             this.lblTarget.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblTarget.Location = new System.Drawing.Point(82, 50);
+            this.lblTarget.Location = new System.Drawing.Point(77, 86);
             this.lblTarget.Name = "lblTarget";
             this.lblTarget.Size = new System.Drawing.Size(23, 25);
             this.lblTarget.TabIndex = 5;
@@ -104,7 +112,7 @@
             // 
             this.lblInfoNumber.AutoSize = true;
             this.lblInfoNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblInfoNumber.Location = new System.Drawing.Point(12, 12);
+            this.lblInfoNumber.Location = new System.Drawing.Point(7, 48);
             this.lblInfoNumber.Name = "lblInfoNumber";
             this.lblInfoNumber.Size = new System.Drawing.Size(69, 25);
             this.lblInfoNumber.TabIndex = 6;
@@ -114,7 +122,7 @@
             // 
             this.lblInfoTarget.AutoSize = true;
             this.lblInfoTarget.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblInfoTarget.Location = new System.Drawing.Point(12, 46);
+            this.lblInfoTarget.Location = new System.Drawing.Point(7, 82);
             this.lblInfoTarget.Name = "lblInfoTarget";
             this.lblInfoTarget.Size = new System.Drawing.Size(59, 25);
             this.lblInfoTarget.TabIndex = 7;
@@ -124,7 +132,7 @@
             // 
             this.lblInfoStep.AutoSize = true;
             this.lblInfoStep.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblInfoStep.Location = new System.Drawing.Point(12, 82);
+            this.lblInfoStep.Location = new System.Drawing.Point(7, 118);
             this.lblInfoStep.Name = "lblInfoStep";
             this.lblInfoStep.Size = new System.Drawing.Size(57, 25);
             this.lblInfoStep.TabIndex = 8;
@@ -134,17 +142,78 @@
             // 
             this.lblStep.AutoSize = true;
             this.lblStep.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblStep.Location = new System.Drawing.Point(82, 82);
+            this.lblStep.Location = new System.Drawing.Point(77, 118);
             this.lblStep.Name = "lblStep";
             this.lblStep.Size = new System.Drawing.Size(23, 25);
             this.lblStep.TabIndex = 9;
             this.lblStep.Text = "0";
             // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(12, 155);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 10;
+            this.btnCancel.Text = "Отмена";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(450, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.новаяИграToolStripMenuItem,
+            this.отменитьToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // новаяИграToolStripMenuItem
+            // 
+            this.новаяИграToolStripMenuItem.Name = "новаяИграToolStripMenuItem";
+            this.новаяИграToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.новаяИграToolStripMenuItem.Text = "Новая игра";
+            this.новаяИграToolStripMenuItem.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // отменитьToolStripMenuItem
+            // 
+            this.отменитьToolStripMenuItem.Name = "отменитьToolStripMenuItem";
+            this.отменитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.отменитьToolStripMenuItem.Text = "Отменить";
+            this.отменитьToolStripMenuItem.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lbHistory
+            // 
+            this.lbHistory.FormattingEnabled = true;
+            this.lbHistory.Location = new System.Drawing.Point(275, 48);
+            this.lbHistory.Name = "lbHistory";
+            this.lbHistory.Size = new System.Drawing.Size(163, 95);
+            this.lbHistory.TabIndex = 12;
+            // 
             // Doubler
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(243, 154);
+            this.ClientSize = new System.Drawing.Size(450, 190);
+            this.Controls.Add(this.lbHistory);
+            this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.lblStep);
             this.Controls.Add(this.lblInfoStep);
             this.Controls.Add(this.lblInfoTarget);
@@ -155,8 +224,12 @@
             this.Controls.Add(this.btnReset);
             this.Controls.Add(this.btnCommand2);
             this.Controls.Add(this.btnCommand1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Doubler";
-            this.Text = "Form1";
+            this.Text = "Удвоитель";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -174,6 +247,13 @@
         private System.Windows.Forms.Label lblInfoTarget;
         private System.Windows.Forms.Label lblInfoStep;
         private System.Windows.Forms.Label lblStep;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem новаяИграToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem отменитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ListBox lbHistory;
     }
 }
 
--- HomeTask7/Doubler.cs
+++ HomeTask7/Doubler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,18 +11,60 @@ using System.Windows.Forms;
 
 namespace HomeTask7
 {
+    /// <summary>
+    /// Объект сохранения действий
+    /// </summary>
+    public struct EventBtn
+    {
+        /// <summary>
+        /// значение вначале действия
+        /// </summary>
+        public int first;
+        /// <summary>
+        /// Шаг на котором совержено действие
+        /// </summary>
+        public int step;
+        /// <summary>
+        /// Тип совершенного действия
+        /// </summary>
+        public char method;
+    }
+
+    /// <summary>
+    /// Форма игры удвоитель
+    /// </summary>
     public partial class Doubler : Form
     {
-        int CommandCount=0;
-        int UserNumber=1;
+        /// <summary>
+        /// Счетчик шагов
+        /// </summary>
+        int CommandCount = 0;
+        /// <summary>
+        /// Текущее число
+        /// </summary>
+        int UserNumber = 1;
+        /// <summary>
+        /// Целевое чесло которое нужно достигнуть путем выполнения действий
+        /// </summary>
         int TargetNumber;
-        Stack;
-//        private delegate int Function();
+        /// <summary>
+        /// Список выполненых дейятвий
+        /// </summary>
+        List<EventBtn> History = new List<EventBtn>();
+        Stack ts = new Stack();
+        //        private delegate int Function();
+        /// <summary>
+        /// Увеличение шага
+        /// </summary>
+        /// <returns></returns>
         private int Increase()
         {
             return ++CommandCount;
         }
-
+        /// <summary>
+        /// обнуление шага
+        /// </summary>
+        /// <returns></returns>
         private int Zero()
         {
             return 0;
@@ -30,48 +73,117 @@ namespace HomeTask7
         public Doubler()
         {
             InitializeComponent();
+            EventBtn HisTemp;
+            HisTemp.first = 1;
+            HisTemp.step = 0;
+            HisTemp.method = '_';
+            History.Add(HisTemp);
         }
 
+        /// <summary>
+        /// Получение случайного числа от 0 до 1000
+        /// </summary>
+        /// <returns></returns>
         private int GetNumber()
         {
             Random rnd = new Random();
-                int number = rnd.Next(1, 1000);
+            int number = rnd.Next(1, 1000);
             return number;
         }
+        /// <summary>
+        /// Делегат для работы со счетчиком шагов
+        /// </summary>
+        /// <returns></returns>
         private delegate int Function();
+        /// <summary>
+        /// Изменение значение счетчика шагов
+        /// </summary>
+        /// <param name="F">Увеличить или обнулить шаг</param>
         private void ChangeSteps(Function F)
         {
             CommandCount = F();
             lblStep.Text = Convert.ToString(CommandCount);
         }
+
+        private delegate void Event(object sender, EventArgs e);
+
+        /// <summary>
+        /// Увеличение числа на 1
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnCommand1_Click(object sender, EventArgs e)
         {
+            EventBtn HisTemp;
             ChangeSteps(Increase);
             UserNumber++;
+
+            HisTemp.first = UserNumber;
+            HisTemp.step = CommandCount;
+            HisTemp.method = '+';
+            History.Add(HisTemp);
+
             lblNumber.Text = Convert.ToString(UserNumber);
-        }
-        private void btnCommand2_Click(object sender, EventArgs e)
-        {
-            ChangeSteps(Increase);
-            UserNumber *= 2;
-            lblNumber.Text = Convert.ToString(UserNumber);
+            lbHistory.Items.Add(1 + lbHistory.Items.Count + " Инкремент: число  " + HisTemp.first + "  шаг " + HisTemp.step);
+
+            ts.Push(HisTemp);
         }
 
+        /// <summary>
+        /// Увеличение числа в два раза
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCommand2_Click(object sender, EventArgs e)
+        {
+            EventBtn HisTemp;
+            ChangeSteps(Increase);
+            UserNumber *= 2;
+
+            HisTemp.first = UserNumber;
+            HisTemp.step = CommandCount;
+            HisTemp.method = '*';
+            History.Add(HisTemp);
+
+            lblNumber.Text = Convert.ToString(UserNumber);
+            lbHistory.Items.Add(1 + lbHistory.Items.Count + " Умножение: число " + HisTemp.first + "  шаг " + HisTemp.step);
+            ts.Push(HisTemp);
+        }
+
+        /// <summary>
+        /// Сброс всех изменений
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnReset_Click(object sender, EventArgs e)
         {
             ChangeSteps(Zero);
             UserNumber = 1;
             lblNumber.Text = Convert.ToString(UserNumber);
+            ts.Clear();
+            lbHistory.Items.Clear();
+            History.Clear();
+
+            EventBtn HisTemp;
+            HisTemp.first = 1;
+            HisTemp.step = 0;
+            HisTemp.method = '_';
+            History.Add(HisTemp);
         }
 
+        /// <summary>
+        /// Начало новой игры
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnPlay_Click(object sender, EventArgs e)
         {
             int Target = GetNumber();
             TargetNumber = Target;
             UserNumber = 1;
-            string text = "Да начнется игра!\n" + "Ваше число : " + Target 
+            string text = "Да начнется игра!\n" + "Ваше число : " + Target
                 + "\n Получите его за минимальное количество ходов";
-                                 
+
             MessageBox.Show(text);
 
             ChangeSteps(Zero);
@@ -79,6 +191,35 @@ namespace HomeTask7
             lblTarget.Text = Convert.ToString(Target);
         }
 
+        /// <summary>
+        /// Отмена последнего действия
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if (ts.Count > 0)
+            {
+                ts.Pop();
+                lbHistory.Items.RemoveAt(lbHistory.Items.Count - 1);
+                History.RemoveAt(History.Count - 1);
+                if (History.Count > 0)
+                {
+                    lblNumber.Text = Convert.ToString(UserNumber = History[History.Count - 1].first);
+                    lblStep.Text = Convert.ToString(CommandCount = History[History.Count - 1].step);
+                }
+            }
 
+        }
+
+        /// <summary>
+        /// Закрытие формы
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
